f62eac77010a560b354f35c2457f32d10c55b2fc
Keomony <keomony@gmail.com>
2017-10-02 21:35:20 +0100

    Welcome Message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,16 @@
 package com.twu.biblioteca;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.run(System.in,System.out);
+    }
+
+    public void run(InputStream in, PrintStream out) {
+        out.println("Welcome");
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void testWelcomeMessage(){
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+
+        bibliotecaApp.run(null, ps);
+
+        assertThat(baos.toString(), containsString("Welcome"));
+    }
+}
